6455ab42de9371a883db3d7be9ce5d93e8d19cd2
oleg61 <oleg.allowweb@gmail.com>
2018-06-30 12:53:55 +0300

    Рисуем пингвинов
    Напишите программу, которая по данному числу N от 1 до 9 выводит на экран N пингвинов. Изображение одного пингвина имеет размер 5×9 символов, между двумя соседними пингвинами также имеется пустой (из пробелов) столбец. Разрешается вывести пустой столбец после последнего пингвина.

---
+++ penguin2.py
@@ -0,0 +1,11 @@
+a = '   _~_    '
+b = '  (o o)   '
+c = ' /  V  \\  '
+d = '/(  _  )\\ '
+e = '  ^^ ^^   '
+req = int(input())
+print(a * req)
+print(b * req)
+print(c * req)
+print(d * req)
+print(e * req)
